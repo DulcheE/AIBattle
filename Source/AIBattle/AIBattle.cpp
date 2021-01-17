e1c52c42a52f0018530d13b64b76d92b43572e4c
Eddy <eddy.dulche@live.fr>
2021-01-17 14:46:50 +0100

    Initial commit

---
+++ Source/AIBattle/AIBattle.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "AIBattle.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AIBattle, "AIBattle" );
---
+++ Source/AIBattle/AIBattleGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIBattleGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIBATTLE_API AAIBattleGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
